c05f20636b2d2840bb643434bf120886a77ebe5f
haru <h.ogawa.3014@gmail.com>
2026-03-30 16:50:02 +0900

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,17 @@
+fetch("header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
+
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+fetch("otoiawase.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("otoiawase").innerHTML = data;
+  });
